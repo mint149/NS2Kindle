0f480a0b5053ff6065ca3ab32e9354d7b8b793dd
Mint <skylove.exe@gmail.com>
2016-09-24 11:07:52 +0900

    スクレイピングしてHTMLファイル生成まで実装

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ tweets = root.cssselect('.tweet')
 for tweet in tweets:
 	print(tweet.text_content())
 
+# ファイル生成
+header = open('header.html', 'r')
+footer = open('footer.html', 'r')
+output = open('_output.html', 'w')
+
+for tag in header.readlines():
+	print(tag)
+	output.write(tag)
+
+title = '<h2>**TITLE**</h2><hr><br>'
+title = title.replace('**TITLE**', 'バック・イン・ブラック')
+output.write(title)
+
+for tweet in tweets:
+	tweetStr = '<p>' + tweet.text_content() + '</p><br>'
+
+	print(tweetStr)
+	output.write(tweetStr)
+
+for tag in footer.readlines():
+	print(tag)
+	output.write(tag)
